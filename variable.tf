5aa0ed16d2961b7d7f87c4bd055a9f2ede5ab571
hameed <40880601+hamsyed@users.noreply.github.com>
2021-05-24 13:30:18 +1000

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,6 +1,26 @@
-variable "region" {
-  default = "us-east-1"
+variable "region" {  
+  default = "us-east-1"  
+}  
+variable "vpc_cidr" {  
+  default = "10.20.0.0/16"  
+}  
+  
+variable "instance_type" {
+  default = "t2.micro"
 }
-variable "vpc_cidr" {
-  default = "10.20.0.0/16"
+
+variable "image" {
+  type = map
+  default = {
+    us-east-1 = "ami-00a9d4a05375b2763"
+    us-east-2 = "ami-00d1f8201864cc10c"
 }
+}
+
+variable "tags" {
+  type = map  
+  default = {  
+    Name = "webserver"  
+}  
+}  
+~               
