ab0e68c078b2ef0b535a478190fa36f4c686b4a7
hameed <40880601+hamsyed@users.noreply.github.com>
2021-05-25 20:34:01 +1000

    Update auto.tf

--- auto.tf
+++ auto.tf
@@ -2,7 +2,9 @@ resource "aws_autoscaling_group" "web_auto" {
   name                 = "web_scaling"
   availability_zones        = ["us-east-1a"]
   launch_configuration = "${aws_launch_configuration.apache-webserver.name}"
-  health_check_grace_period = 3000
+  load_balancers = [ aws_elb.web-elb.name ]
+  health_check_type = "ELB"
+  health_check_grace_period = 300
   min_size             = 1
   max_size             = 2
 
