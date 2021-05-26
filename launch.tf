e008eed998539fe1cca566a2acb92f400a8edcaa
hameed <40880601+hamsyed@users.noreply.github.com>
2021-05-26 18:35:12 +1000

    Update launch.tf

--- launch.tf
+++ launch.tf
@@ -1,9 +1,36 @@
 resource "aws_launch_configuration" "apache-webserver" {
   name = "webserver"
   image_id      = "ami-0d5eff06f840b45e9"
-  instance_type = "t2.micro"
+  instance_type = "${var.instance_type}"
   associate_public_ip_address = "true"
+  key_name = "terraform.pem" 
   user_data = "${file("bash.sh")}"
+
+  provisioner "local-exec" {
+    command = "/bin/bash bash.sh"
+
+  environment = {
+    Name = "${var.tags["Name"]}"
+    }
+  }
+
+  provisioner "file" {
+  source      = ". bash.sh"
+  destination = "/home/ec2-user"
+
+  connection {
+    type     = "ssh"
+    user     = "ec2-user"
+    host     = "self.public_ip"
+    private_key = file(("./terraform.pem"))
+  }
+    
+  provisioner "remote-exec" {
+    inline = [
+      "sudo cd /home/ec2-user",
+      "sudo ./bash.sh",
+       ]
+}  
 
   lifecycle {
     create_before_destroy = true
